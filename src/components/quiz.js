9d55f4ec122f6e9e875fca04cb1b12baa6b972d3
Manuel Moya <moya.manuel41@gmail.com>
2019-02-04 13:09:53 -0800

    Add 'Quiz' component and CSS module for Quiz component

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import styles from '../components/quiz.module.css'
 
 class Quiz extends React.Component {
+    retrieveWordBank() {
+        let answerBankItems = this.props.answerBank.map((word) => {
+            <li>{word}</li>
+        })
+        console.log(answerBankItems)
+        return answerBankItems
+    }
     render() {
+        let answerBankItems = this.props.answerBank.map((word) => {
+            <li>{word}</li>
+        })
         return (
-            <div>
-                <h1>{this.props.questions}</h1>
+            <div className={styles.card}>
+                <h1>{this.props.question}</h1>
+                <ul>
+                    {answerBankItems}
+                </ul>
             </div>
         )
     }
