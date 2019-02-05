e356c3ec45f9f43ad9bb9fbc3978cfe12e818304
Manuel Moya <moya.manuel41@gmail.com>
2019-02-04 17:46:02 -0800

    Add 'contact' route

---
+++ src/pages/contact.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+    <div>
+        <h2>Contact Information</h2>
+        <a href="mailto:manuelmoyadev@gmail.com?subject=CS-11%20Question&body=Hey Manuel -- I have a question regarding [something]. I've tried [what you tried] so far, but I'm still having trouble with [give the details]">Email</a>
+    </div>
+)
