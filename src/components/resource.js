487a136a23537a99f0c7c6a816fa84079db0e9c1
Manuel Moya <moya.manuel41@gmail.com>
2019-02-04 17:45:35 -0800

    Retrieve and display resource list in Resource component

--- src/components/resource.js
+++ src/components/resource.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
-export default (props) => (
-    <div>
-        <h1>{props.week}</h1>
-        <p>{props.resources}</p>
-    </div>
-)
+export default (props) => {
+    let resourceList = props.resources.map((resource) => 
+        <li>{resource}</li>
+    )
+
+    return (
+        <div>
+            <h1>{props.week}</h1>
+            <p>{props.resources}</p>
+            <ul>
+                {resourceList}
+            </ul>
+        </div>
+    )
+}
