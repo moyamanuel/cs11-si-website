6b6a7f07d426dbcf05940913ceacca6245baa63a
Manuel Moya <moya.manuel41@gmail.com>
2019-04-10 15:40:40 -0700

    Add Calendar component in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,20 @@
 import React from 'react'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
-import BigCalendar from 'react-big-calendar'
-import moment from 'moment'
-import Layout from "../components/layout"
-
-require('react-big-calendar/lib/css/react-big-calendar.css');
-
-//import MyCalendar from '../components/test'
-
-const localizer = BigCalendar.momentLocalizer(moment)
-
-
-
+import Layout from '../components/layout'
+import Calendar from '../components/calendar'
 class RootIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
 
     return (
       <Layout>
-        <div style={{ background: '#fff', width:'100%', height:'100%'}}>
+        <div style={{ background: '#fff'}}>
         <Helmet title={siteTitle} />
         <h1>This is the start of something great</h1>
-          <BigCalendar
-            localizer={localizer}
-            events={[]}
-            startAccessor="start"
-            endAccessor="end"
-          />
+          <div style={{height:'500px'}}>
+            <Calendar/>
+          </div>
         </div>
       </Layout>
     )
