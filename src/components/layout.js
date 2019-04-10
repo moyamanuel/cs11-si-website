460ff2885420b11013d52581037c07d5661577e5
Manuel Moya <moya.manuel41@gmail.com>
2019-04-10 15:35:48 -0700

    Refactor Layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,11 @@
-import React from "react"
+import React from 'react'
+import Navigation from '../components/navigation'
 import 'normalize.css';
-import "./base.css"
+import './base.css'
 
-export default ({ children }) => <div>{children}</div>
+export default ({ children }) => (
+  <div id="test" style={{ width:'100%', height:'100%'}}>
+    <Navigation/>
+    {children}
+  </div>
+)
