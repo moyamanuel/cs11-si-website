dccb25829fc72bb9c9712d769d765bb4e3402e35
Manuel Moya <moya.manuel41@gmail.com>
2019-04-12 14:42:54 -0700

    Remove Navbar until pages are fully implemented

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import './base.css'
 
 export default ({ children }) => (
   <div id="test" style={{ width:'100%', height:'100%'}}>
-    <Navigation/>
+    {/* <Navigation/> */}
     {children}
   </div>
 )
