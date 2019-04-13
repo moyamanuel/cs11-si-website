d64f55f7eddd36916dd9d35f8e43c6eacd0395a7
Manuel Moya <moya.manuel41@gmail.com>
2019-04-13 13:30:15 -0700

    Fix calendar loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,17 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Calendar from '../components/calendar'
 class RootIndex extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      test: null,
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      test: <Calendar/>
+    })
+  }
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
 
@@ -13,7 +24,7 @@ class RootIndex extends React.Component {
         <Helmet title={siteTitle} />
         <h1>This is the start of something great</h1>
           <div style={{height:'500px'}}>
-            <Calendar/>
+            {this.state.test}
           </div>
         </div>
       </Layout>
