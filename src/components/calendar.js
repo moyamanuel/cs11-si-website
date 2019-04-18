e2550ad2fede9820c28e50356fb2cc2c143fb93a
Manuel Moya <moya.manuel41@gmail.com>
2019-04-18 15:39:02 -0700

    Add componentDidMount method to index page

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,44 +1,16 @@
 import React from "react"
 import BigCalendar from 'react-big-calendar'
 import moment from 'moment'
-import { StaticQuery, graphql } from "gatsby"
 
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 const localizer = BigCalendar.momentLocalizer(moment)
 
-export default () => {
+export default (props) => {
   return (
-    <StaticQuery
-      query={graphql`
-      query CalendarEvents {
-          allContentfulSiSession {
-            edges {
-              node {
-                title
-                start
-                end
-                isAllDay
-              }
-            }
-          }
-        }
-      `}
-      render={data => {
-        let eventList = []
-        data.allContentfulSiSession.edges.forEach(eventObject => {
-          eventObject.node.start = new Date(eventObject.node.start)
-          eventObject.node.end = new Date(eventObject.node.end)
-          eventList.push(eventObject.node)
-        })
-        console.log(eventList)
-        return (
-          <BigCalendar
-            localizer={localizer}
-            events={eventList}
-          />
-        )
-      }}
+    <BigCalendar
+      localizer={localizer}
+      events={props.eventList}
     />
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,33 +3,55 @@ import get from 'lodash/get'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Calendar from '../components/calendar'
+
 class RootIndex extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      test: null,
+      calendarEvents: [],
     }
   }
+
   componentDidMount() {
-    this.setState({
-      test: <Calendar/>
+    let allCalendarEvents = this.props.data.allContentfulSiSession.edges;
+    allCalendarEvents.forEach(eventObject => {
+      eventObject.node.start = new Date(eventObject.node.start)
+      eventObject.node.end = new Date(eventObject.node.end)
+      this.setState(prevState => ({
+        calendarEvents: [...prevState.calendarEvents, eventObject.node]
+      }))
     })
   }
+
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-
     return (
       <Layout>
         <div style={{ background: '#fff'}}>
         <Helmet title={siteTitle} />
         <h1>This is the start of something great</h1>
           <div style={{height:'500px'}}>
-            {this.state.test}
+            <Calendar eventList={this.state.calendarEvents}/>
           </div>
         </div>
       </Layout>
     )
   }
 }
+
+export const query = graphql`
+  query allCalendarEvents {
+    allContentfulSiSession {
+      edges {
+        node {
+          title
+          start
+          end
+          isAllDay
+        }
+      }
+    }
+  }
+`
 
 export default RootIndex
